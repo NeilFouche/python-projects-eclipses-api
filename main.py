e209bed02297e95eae3e8998520a8b75f6b2e972
NeilFouche <neilfouche@rocketmail.com>
2025-10-02 19:07:31 +0200

    Created an endpoint for the landing

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Eclipses API
+by Neil Fouche
+
+A simple FastAPI application to serve data on upcoming lunar and solar eclipses.
+
+Host: render.com @ https://eclipses-api.onrender.com
+"""
+
 import pandas as pd
 from fastapi import FastAPI, HTTPException
 from typing import List, Optional
@@ -9,18 +18,20 @@ app = FastAPI(
     description="A simle API for eclipse data"
 )
 
+@app.get("/", response_model=Eclipse)
+async def home():
+    return find_next_eclipse()
+
+
 @app.get("/eclipses", response_model=List[Eclipse])
 async def get_all_eclipses():
     return get_eclipses()
 
 @app.get("/eclipses/next", response_model=Eclipse)
 async def get_next_eclipse():
-    now = datetime.now().date()
-    eclipses = get_eclipses()
-    for eclipse in eclipses:
-        eclipse_date = eclipse['date']
-        if eclipse_date >= now:
-            return eclipse
+    next_eclipse = find_next_eclipse()
+    if next_eclipse:
+        return next_eclipse
 
     raise HTTPException(status_code=404, detail="No upcoming eclipses found.")
 
@@ -36,6 +47,15 @@ async def get_eclipses_by_type(eclipse_type: str):
         HTTPException(status_code=404, detail=f"No {eclipse_type} eclipses found.")
 
     return eclipses
+
+def find_next_eclipse():
+    now = datetime.now().date()
+    eclipses = get_eclipses()
+    for eclipse in eclipses:
+        eclipse_date = eclipse['date']
+        if eclipse_date >= now:
+            return eclipse
+    return None
 
 def get_eclipses(eclipse_type: Optional[str] = None):
     df = pd.read_csv("eclipse_data.csv", delimiter=";")
